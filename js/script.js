e0bd57e25095945dbaae6fb2d0345f43d5c4a6b8
ChenazAdams <chenazadams3@gmail.com>
2021-03-02 10:54:16 +0100

    footer icons, jquery, responsive hamburger menu
    CDN link, fontawesome icons, responsive hamburger menu

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Hi, welcome to my UX Portfolio");
+console.log('hi everyone, welcome to my portfolio site');
+console.log("UX/UI Designer and Content Creator, based in Munich");
+
+var text1 = "Hey there!";
+var text2 = "UX/UI";
+
+var escapedText1 = "They say\"absolutely!\"why not?";
+var escapedText2 = "He said: that\'s great!";
+
+console.log(numberInteger);
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
